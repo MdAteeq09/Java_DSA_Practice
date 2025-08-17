6c777384e733e20d4e224a113cf45c2de17fe6d6
Ateeq <mdateequrrahma123@gmail.com>
2025-08-17 23:06:41 +0530

    beautifulAl added

--- Arrays/beautifulAL.java
+++ Arrays/beautifulAL.java
@@ -24,7 +24,7 @@ public class beautifulAL {
         beautifulAL obj = new beautifulAL();
         ArrayList<Integer> result = obj.beautifulArray(n);
 
-        // Print the result of beautiful 
+        // Print the result of beautiful ArrayListt
         System.out.println("Beautiful ArrayList: " + result);
 
         sc.close();
