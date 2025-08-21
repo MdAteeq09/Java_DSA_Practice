1b2c4fd238c2647ad4ce0d8bad7aa945da9bcee7
Ateeq <mdateequrrahma123@gmail.com>
2025-08-21 22:42:21 +0530

    oublly Linked_list problem added

--- Linked_List/DoubllyLL.java
+++ Linked_List/DoubllyLL.java
@@ -77,7 +77,7 @@ public class DoubllyLL {
     public static void main(String[] args) {
         DoubllyLL dll = new DoubllyLL();
 
-        //add nodes in LL
+        //add nodes in Linked
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
