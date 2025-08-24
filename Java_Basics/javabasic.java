0165e846bc7ff7b121b073b2da543acfe50465f3
Ateeq <mdateequrrahma123@gmail.com>
2025-08-24 23:27:16 +0530

    new file added

--- Java_Basics/javabasic.java
+++ Java_Basics/javabasic.java
@@ -11,7 +11,6 @@ public class javabasic {
         float total = pen + pencil + eraser;
         System.out.println("The total bill is: " + total);
 
-        // Adding 18% GST
         float newTotal = total + (0.18f * total);
         System.out.println("Total with GST: " + newTotal);
     }
