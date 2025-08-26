7d7e3c7224891fc18aa057de7eb7204e8304556c
Ateeq <mdateequrrahma123@gmail.com>
2025-08-26 23:29:13 +0530

    linklist problem added

--- Linked_List/LinkList.java
+++ Linked_List/LinkList.java
@@ -391,7 +391,7 @@ public class LinkList {
 
     public static void main(String[] args) {
     LinkList ll = new LinkList();
-    //add nodes in
+    //add nodes in link
     ll.addLast(1);
     ll.addLast(2);
     ll.addLast(3);
