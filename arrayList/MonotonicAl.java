d581a333852943151decb19c7cfd6c20e99f232d
Ateeq <mdateequrrahma123@gmail.com>
2025-09-02 22:54:16 +0530

    MonotonicAL problem added

---
+++ arrayList/MonotonicAl.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MonotonicAl {
+
+    public static boolean isMonotonic(ArrayList<Integer> A) {
+        boolean inc = true;
+        boolean dec = true;
+
+        for(int i=0; i<A.size()-1; i++) {
+            if (A.get(i) < A.get(i+1)) {
+                inc = false;
+            } 
+            if(A.get(i) > A.get(i+1)) {
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+  public static void main(String[] args) {
+    ArrayList<Integer> A = new ArrayList<>();
+    A.add(6);
+    A.add(2);
+    A.add(5);
+    System.out.println(isMonotonic(A));
+  }
+}
