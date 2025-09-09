421b16e850c51cf45e0f38d3e76d60166b796885
Ateeq <mdateequrrahma123@gmail.com>
2025-09-09 23:43:04 +0530

    rotatingArray problem added

--- Arrays/rotatingarray.java
+++ Arrays/rotatingarray.java
@@ -26,7 +26,6 @@ public class rotatingarray {
         
         //mid on line 2
         else{
-            //case c : right
              if(arr[mid] <= tar && tar <= arr[ei]) {
               return search(arr, tar, mid+1, ei);
             }else {
