9ae821a4aa7369de7c667bed55678ea14eac5cc7
Ateeq <mdateequrrahma123@gmail.com>
2025-09-13 23:13:21 +0530

    mergeString problem added

--- string/stringmerge.java
+++ string/stringmerge.java
@@ -39,7 +39,7 @@ public class stringmerge {
         return arr3;
     }
 
-    // This method should be outside `merge`
+    // This method should be outside 
     public static boolean isAlphabeticallySmaller(String str1, String str2) {
         return str1.compareTo(str2) < 0;
     }
