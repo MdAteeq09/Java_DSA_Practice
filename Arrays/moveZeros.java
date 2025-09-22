33b7ba7d903c421899b0f8926c65556996f6bcb3
Ateeq <mdateequrrahma123@gmail.com>
2025-09-22 23:55:41 +0530

    moveZeros problem added

--- Arrays/moveZeros.java
+++ Arrays/moveZeros.java
@@ -16,7 +16,7 @@ class moveZeros {
         }       
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) {//
         int[] nums = {0, 1, 0, 3, 12};
         
         moveZeroes(nums);
