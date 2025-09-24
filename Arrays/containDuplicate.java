3649226bb0656e64b1845506fe8d3b1ebde96886
Ateeq <mdateequrrahma123@gmail.com>
2025-09-24 23:20:10 +0530

    ContainDuplicate problem added

---
+++ Arrays/containDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class containDuplicate {
+     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // duplicate mil gaya
+            }
+            seen.add(num);
+        }
+        return false; // koi duplicate nahi mila
+    }
+
+    public static void main(String[] args) {
+        containDuplicate sol = new containDuplicate();
+        int[] arr1 = {1, 2, 3, 1};
+        int[] arr2 = {1, 2, 3, 4};
+
+        System.out.println(sol.containsDuplicate(arr1)); // true
+        System.out.println(sol.containsDuplicate(arr2)); // false
+    }
+}
