f16ed9b2320296a6ed26b9fbb36f0badf9bcfa35
Ateeq <mdateequrrahma123@gmail.com>
2025-09-30 21:56:24 +0530

    stock2 problem added

--- Arrays/stock2.java
+++ Arrays/stock2.java
@@ -12,6 +12,6 @@ public class stock2 {
     public static void main(String[] args) {
         stock2 sol = new stock2();
         int arr[] = {7,1,5,3,6,4};
-        System.out.print(sol.maxProfit(arr));
+        System.out.print(sol.maxProfit(arr));//
     }
 }
