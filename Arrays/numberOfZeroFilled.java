ab318477729584b374088f73c468fdfdf87de302
Ateeq <mdateequrrahma123@gmail.com>
2025-10-09 23:26:07 +0530

    nofilledwithZero problem added

---
+++ Arrays/numberOfZeroFilled.java
@@ -0,0 +1,22 @@
+public class numberOfZeroFilled {
+    public long zeroFilledSubarray(int[] nums) {
+        long total = 0; 
+        long count = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            if (nums[i] == 0) {
+                count ++;
+                total = total + count;
+            } else {
+                count = 0;
+            }
+        }
+        return total;
+    }
+    
+    public static void main(String [] args) {
+        numberOfZeroFilled sol = new numberOfZeroFilled();
+        int nums [] = {1,3,0,0,2,0,0,4};
+        System.out.print(sol.zeroFilledSubarray(nums));
+    } 
+}
