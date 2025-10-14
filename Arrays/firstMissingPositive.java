d2f9c655ef3ca72b1d5843355d1d3b0b67b899ad
Ateeq <mdateequrrahma123@gmail.com>
2025-10-14 23:49:37 +0530

    firstMissingPositive problem added

---
+++ Arrays/firstMissingPositive.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class firstMissingPositive {
+     public static int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int expected = 1;
+        
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == expected) {
+                expected ++;
+            }
+        }
+        return expected;
+    }
+    public static void main(String [] args) {
+        int nums[] = {7,8,9,11,12};
+        System.out.println(firstMissingPositive(nums));
+    }
+}
