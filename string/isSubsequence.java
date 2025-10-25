de69c1b94894926380dad4606bde71836ca63263
Ateeq <mdateequrrahma123@gmail.com>
2025-10-25 23:04:59 +0530

    isSubsequence problem added

---
+++ string/isSubsequence.java
@@ -0,0 +1,18 @@
+public class isSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i =0;
+        int j= 0;
+        
+        while( i<s.length() && j<t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+        j++;
+        }
+        return i == s.length();
+    }
+    public static void main(String[] args) {
+        isSubsequence sol = new isSubsequence();
+        System.out.println(sol.isSubsequence("abc", "ahbgdc"));
+    }
+}
