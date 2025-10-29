c363344d407a8a23813aa8bf553acc3a1f52639a
Ateeq <mdateequrrahma123@gmail.com>
2025-10-29 23:12:48 +0530

    singleNumber problem added

---
+++ string/singleNumber.java
@@ -0,0 +1,14 @@
+public class singleNumber {
+    public int singleNumber(int[] nums) {
+        int res =0;
+        for(int i=0; i<nums.length; i++) {
+            res = nums[i] ^ res;
+        }
+        return res;
+    }
+    public static void main(String [] args) {
+        singleNumber sol = new singleNumber();
+        int [] nums = {2,2,1};
+        System.out.println(sol.singleNumber(nums));
+    }
+}
