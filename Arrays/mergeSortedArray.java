8ad2b9a881df1285ea42d55d254b6a6a7e4c838d
Ateeq <mdateequrrahma123@gmail.com>
2025-10-31 22:32:31 +0530

    mergeSortedArray problem added

---
+++ Arrays/mergeSortedArray.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class mergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = m+n-1, i= m-1, j= n-1;
+        
+        while(i>=0 && j>=0) {
+            if(nums1[i] >= nums2[j]) {
+                nums1[idx] = nums1[i];
+                idx--;
+                i--;
+            } else  {
+                nums1[idx] = nums2[j];
+                idx--;
+                j--;
+            }
+        }
+        
+        while (j>=0) {
+            nums1[idx] = nums2[j];
+            idx--;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        mergeSortedArray sol = new mergeSortedArray();
+        int[] nums1 = {1,2,3,0,0,0};  
+        int[] nums2 = {2,5,6}; 
+        int m=3, n=3;
+
+        sol.merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));   
+    }
+}
