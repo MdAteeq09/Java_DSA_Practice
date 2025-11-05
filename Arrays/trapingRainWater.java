74a454b2032c58676d2cb34f0700d6161d25c0f4
Ateeq <mdateequrrahma123@gmail.com>
2025-11-05 23:46:18 +0530

    trapingRainWater problem added

---
+++ Arrays/trapingRainWater.java
@@ -0,0 +1,25 @@
+public class trapingRainWater {
+    public int trap(int[] height) {
+        int n = height.length, ans =0, l=0, r=n-1, lm=0, rm=0;
+
+        while(l < r) {
+
+            lm = Math.max(lm, height[l]);
+            rm = Math.max(rm, height[r]);
+
+            if(lm < rm) {
+                ans += lm - height[l];
+                l++;
+            } else {
+                ans += rm - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+    public static void main(String [] args) {
+        trapingRainWater sol = new trapingRainWater();
+        int ht [] = {4,2,0,3,2,5};
+        System.out.print(sol.trap(ht));
+    }
+}
