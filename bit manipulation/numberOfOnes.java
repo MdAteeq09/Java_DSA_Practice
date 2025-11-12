8d25c23b9fa5ca1ad78786e24cefde559345f62d
Ateeq <mdateequrrahma123@gmail.com>
2025-11-12 23:51:43 +0530

    numberOfOnes problem added

---
+++ bit manipulation/numberOfOnes.java
@@ -0,0 +1,18 @@
+public class numberOfOnes {
+     public int hammingWeight(int n) {
+        String binary = Integer.toBinaryString(n);
+        int count = 0;
+
+        for(int i=0; i<binary.length(); i++) {
+            if(binary.charAt(i) == '1') {
+                count++;
+            }
+        }
+      
+        return count;
+    } public static void main(String [] agrs) {
+        int n = 11;
+        numberOfOnes sol = new numberOfOnes();
+        System.out.print(sol.hammingWeight(n));
+    }
+}
