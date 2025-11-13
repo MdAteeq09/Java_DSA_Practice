897fa1304a0fc4f5c63276a133b427e4db6870a8
Ateeq <mdateequrrahma123@gmail.com>
2025-11-13 23:51:46 +0530

    countingBits problem added

---
+++ bit manipulation/countingBits.java
@@ -0,0 +1,19 @@
+public class countingBits {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        
+        return res;
+    }
+    public static void main(String [] args) {
+        int n = 2;
+        countingBits sol = new countingBits();
+        int arr[] = sol.countBits(n);
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] );
+        }
+    }
+}
