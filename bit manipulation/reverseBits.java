ea5fc4c391d3c5265dc08d50b541b1b7c83df0bc
Ateeq <mdateequrrahma123@gmail.com>
2025-11-15 22:39:48 +0530

    reversebits problem added

---
+++ bit manipulation/reverseBits.java
@@ -0,0 +1,17 @@
+public class reverseBits {
+     public static int reverseBits(int n) {
+        int reversed = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            reversed <<= 1;
+            int lastBit = n & 1;
+            reversed |= lastBit;
+            n >>>= 1; 
+        }
+        return reversed;
+    }
+    public static void main(String[] args) {
+        int n = 43261596;
+        System.out.println(reverseBits(n));
+    }
+}
