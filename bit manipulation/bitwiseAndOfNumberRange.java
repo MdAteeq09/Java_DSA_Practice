0bc25229b51de709a789cb3ea727baaf1dbe44f0
Ateeq <mdateequrrahma123@gmail.com>
2025-11-17 22:58:47 +0530

    bitwiseAndOfNumberRange problem added

--- bit manipulation/bitwiseAndOfNumberRange.java
+++ bit manipulation/bitwiseAndOfNumberRange.java
@@ -1,6 +1,6 @@
 public class bitwiseAndOfNumberRange {
      public int rangeBitwiseAnd(int left, int right) {
-      while(right > left) {
+      while(right > left) {// 
         right = right & (right-1);
       }
       return right;
