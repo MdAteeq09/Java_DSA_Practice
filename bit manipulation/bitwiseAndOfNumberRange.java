de2ef401fcff051dd5f273f3911721f226207abc
Ateeq <mdateequrrahma123@gmail.com>
2025-11-17 22:57:59 +0530

    bitwiseAndOfNumberRange problem added

---
+++ bit manipulation/bitwiseAndOfNumberRange.java
@@ -0,0 +1,14 @@
+public class bitwiseAndOfNumberRange {
+     public int rangeBitwiseAnd(int left, int right) {
+      while(right > left) {
+        right = right & (right-1);
+      }
+      return right;
+    }
+    public static void main(String[] args) {
+        bitwiseAndOfNumberRange sol = new bitwiseAndOfNumberRange();
+        int left = 5;
+        int right = 7;
+        System.out.print(sol.rangeBitwiseAnd(left,right));
+    }
+}
