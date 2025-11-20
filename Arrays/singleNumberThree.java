91e7e288b748bd787276f2f0512033fbaf27221d
Ateeq <mdateequrrahma123@gmail.com>
2025-11-20 22:43:23 +0530

    singleNumberThree problem added

---
+++ Arrays/singleNumberThree.java
@@ -0,0 +1,27 @@
+public class singleNumberThree {
+    public static int[] singleNumber(int[] nums) {
+        int res  = 0;
+        for(int i=0; i<nums.length; i++) {
+            res ^= nums[i];
+        }
+        int diff = res & (-res);
+        int a=0, b=0;
+
+        for(int i=0; i<nums.length; i++) {
+            if((nums[i] & diff)==0) {
+                a ^= nums[i];
+            } else {
+                b ^= nums[i];
+            }
+        }
+        return new int[] {a,b};
+    }
+    public static void main(String[] args) {
+        int nums [] = {1,2,1,3,2,5};
+        int res[] = singleNumber(nums);
+
+        for(int i=0; i<res.length; i++) {
+            System.out.print(res[i]);
+        }
+    }
+}
