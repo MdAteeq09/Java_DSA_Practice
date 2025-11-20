f2a60d3debb5575499ea732f77efcd33c6b7f7b1
Ateeq <mdateequrrahma123@gmail.com>
2025-11-20 22:44:13 +0530

    singleNumberThree problem added

--- Arrays/singleNumberThree.java
+++ Arrays/singleNumberThree.java
@@ -18,7 +18,7 @@ public class singleNumberThree {
     }
     public static void main(String[] args) {
         int nums [] = {1,2,1,3,2,5};
-        int res[] = singleNumber(nums);//new array
+        int res[] = singleNumber(nums);//new 
 
         for(int i=0; i<res.length; i++) {
             System.out.print(res[i]);
