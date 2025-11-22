5da38fe66d30260bfd8f181f39f74983b55685fe
Ateeq <mdateequrrahma123@gmail.com>
2025-11-22 21:29:07 +0530

    new file added

--- string/reverseWord.java
+++ string/reverseWord.java
@@ -15,7 +15,7 @@ public class reverseWord {
         return sb.toString();
     }
     public static void main(String[] args) {
-        String str = "hello";
+        String str = "hello world";
         System.out.println(reverseWords(str));
     }
 }
