ab3cdef57d1859af651834282c811d013f77910f
Ateeq <mdateequrrahma123@gmail.com>
2025-11-24 23:25:01 +0530

    new file added

--- 2dArrays/sprialMatrix.java
+++ 2dArrays/sprialMatrix.java
@@ -46,6 +46,6 @@ public class sprialMatrix {
     }
     public static void main(String[] args) {
         int arr [] [] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        System.out.println(spiralOrder(arr));
+        System.out.println(spiralOrder(arr));//
     }
 }
