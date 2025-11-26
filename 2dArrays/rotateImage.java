d1f2b21d60e75ed3f3740d540b9e65f0d81f1047
Ateeq <mdateequrrahma123@gmail.com>
2025-11-26 23:55:19 +0530

    rotate image problem added

--- 2dArrays/rotateImage.java
+++ 2dArrays/rotateImage.java
@@ -27,7 +27,6 @@ public class rotateImage {
 
     public static void main(String [] args) {
         int matrix[][] = {{1,2,3}, {4,5,6,}, {7,8,9}};
-        Solution sol = new Solution();
         rotate(matrix);
     }
 }
