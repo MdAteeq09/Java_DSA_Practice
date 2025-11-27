cfdfa4fa3878c6157ad85c8945c2aed27c432052
Ateeq <mdateequrrahma123@gmail.com>
2025-11-27 22:44:04 +0530

    setZeros problem added

--- 2dArrays/setMatrixZero.java
+++ 2dArrays/setMatrixZero.java
@@ -55,8 +55,11 @@ public class setMatrixZero {
         printMatrix(matrix);
     }
     public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
+        for (int i = 0; i < matrix.length; i++) {                 // row loop
+            for (int j = 0; j < matrix[0].length; j++) {          // column loop
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
