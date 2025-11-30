50b3cf4cabd8958a978905cdaad494298b3b44d9
Ateeq <mdateequrrahma123@gmail.com>
2025-11-30 23:19:44 +0530

    gameOfLife proble added

--- 2dArrays/gameOfLife.java
+++ 2dArrays/gameOfLife.java
@@ -66,7 +66,7 @@ public class gameOfLife {
         printBoard(board);
     }
 
-    // Helper function to print
+    // Helper function to 
     public static void printBoard(int[][] board) {
         for (int[] row : board) {
             System.out.println(Arrays.toString(row));
