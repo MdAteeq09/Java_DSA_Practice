5e24c9f81bdb9de45df1b46feb3ec16adb0e6ec2
Ateeq <mdateequrrahma123@gmail.com>
2025-12-07 23:11:00 +0530

    Kth largest element in array ploblem added

--- Arrays/kthlagrestEleInArr.java
+++ Arrays/kthlagrestEleInArr.java
@@ -18,7 +18,7 @@ public class kthlagrestEleInArr {
     }
     public static void main(String[] args) {
         int nums [] = {3,2,1,5,6,4};
-        int k = 2;
+        int k = 3;
         System.out.println(findKthLargest(nums, k));
     }
 }
