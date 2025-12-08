2636d7424d36e94e7b1ad20db6ef70574305197b
Ateeq <mdateequrrahma123@gmail.com>
2025-12-08 22:50:14 +0530

    third minimum number problem added

--- Arrays/thirdMinNumber.java
+++ Arrays/thirdMinNumber.java
@@ -23,7 +23,7 @@ public class thirdMinNumber {
             }
         }
 
-        // if third max doesn't exist, return first (max)
+      
         return third == Long.MIN_VALUE ? (int) first : (int) third;
     }
     public static void main(String[] args) {
