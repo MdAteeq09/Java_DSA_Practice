32068fbe4b7c3955fcd342d8fb31bfaad2278692
Ateeq <mdateequrrahma123@gmail.com>
2025-12-12 23:40:23 +0530

    nextGreaterElemetTwo problem added

--- stack/nextGreaterEleTwo.java
+++ stack/nextGreaterEleTwo.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class nextGreaterEleTwo {
-     public int[] nextGreaterElements(int[] nums) {
+    public static int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
@@ -29,5 +29,14 @@ public class nextGreaterEleTwo {
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 1 };
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+
     }
 }
