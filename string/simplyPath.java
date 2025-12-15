e74757bb00c4eaac4b4396ec9da3ba5231a339bf
Ateeq <mdateequrrahma123@gmail.com>
2025-12-15 22:28:28 +0530

    simply Path problem added

---
+++ string/simplyPath.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class simplyPath {
+    public static String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] parts = path.split("/");
+
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } 
+            else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } 
+            else {
+                stack.push(part);
+            }
+        }
+        return "/" + String.join("/", stack);
+    }
+}
