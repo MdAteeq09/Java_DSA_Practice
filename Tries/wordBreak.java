255bebb0c2a11805d1d0da855d01309626bc7016
Ateeq <mdateequrrahma123@gmail.com>
2025-12-19 23:50:32 +0530

    word break problem added

--- Tries/wordBreak.java
+++ Tries/wordBreak.java
@@ -50,7 +50,7 @@ public class wordBreak {
     }
 
     public static void main(String[] args) {
-        String arr [] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        String arr [] = {"i", "like", "sam", "samsu", "mobile", "ice"};
         for(int i=0; i<arr.length; i++) {
             insert(arr[i]);
         }
