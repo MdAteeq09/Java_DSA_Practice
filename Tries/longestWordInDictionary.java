098b1f974e2347872f2849bd9294c044b1d50992
Ateeq <mdateequrrahma123@gmail.com>
2025-12-24 23:10:21 +0530

    longest word in dictionary problem added

--- Tries/longestWordInDictionary.java
+++ Tries/longestWordInDictionary.java
@@ -14,7 +14,7 @@ public class longestWordInDictionary {
     public static void insert(String word) {
         Node curr = root;
         for(int level=0; level<word.length(); level++) {
-            int idx = word.charAt(level) - 'ab';
+            int idx = word.charAt(level) - 'a';
             if(curr.children[idx] == null) {
                 curr.children[idx] = new Node();
             }
