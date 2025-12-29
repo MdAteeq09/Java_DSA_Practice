c9f72bb6c3b4452e2840bb0580c0a4484e15baac
Ateeq <mdateequrrahma123@gmail.com>
2025-12-29 22:07:45 +0530

    height of tree problem added

--- binaryTree/heightOfTree.java
+++ binaryTree/heightOfTree.java
@@ -95,7 +95,7 @@ public class heightOfTree {
 
         // System.out.println(height(root));
         // System.out.println(count(root));
-        // System.out.println(sum(root));
+        System.out.println(sum(root));
         System.out.println(diameter2(root));
         System.out.println(diameter(root).diam);
     }
