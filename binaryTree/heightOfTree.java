8a0a60b394ede45f752266fca650f6788a0ccf32
Ateeq <mdateequrrahma123@gmail.com>
2025-12-29 22:07:12 +0530

    height of tree problem added

---
+++ binaryTree/heightOfTree.java
@@ -0,0 +1,102 @@
+public class heightOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh= height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int lcount = count(root.left);
+        int rcount = count(root.right);
+        return lcount + rcount + 1;
+    }
+
+    public static int sum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftsum = sum(root.left);
+        int rightsum = sum(root.right);
+        return leftsum + rightsum + root.data;
+    }
+
+    public static int diameter2(Node root) {//O(n^2)
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDiam = diameter2(root.left);
+        int leftHt = height(root.left);
+        int rightDiam = diameter2(root.right);
+        int rightHt = height(root.right);
+
+        int selfdia = leftHt + rightHt + 1;
+       
+        return Math.max(selfdia, Math.max(rightDiam, leftDiam));
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root) { //O(n)
+        if(root == null) {
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1; 
+
+        return new Info(diam, ht);
+    }
+
+    public static void main(String[] agrs) {
+        /*
+               1
+             /  \
+            2    3
+           / \  / \
+           4  5 6  7
+         */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // System.out.println(height(root));
+        // System.out.println(count(root));
+        // System.out.println(sum(root));
+        // System.out.println(diameter2(root));
+        System.out.println(diameter(root).diam);
+    }
+}
