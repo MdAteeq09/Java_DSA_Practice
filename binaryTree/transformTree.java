13f292ac9ead0c87b5f214acde826c225a2df68a
Ateeq <mdateequrrahma123@gmail.com>
2026-01-07 23:58:09 +0530

    transform tree problem added

--- binaryTree/transformTree.java
+++ binaryTree/transformTree.java
@@ -39,14 +39,7 @@ public class transformTree {
     }
 
     public static void main(String[] agrs) {
-        /*
-         * 1
-         * / \
-         * 2 3
-         * / \ / \
-         * 4 5 6 7
-         */
-
+     
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
