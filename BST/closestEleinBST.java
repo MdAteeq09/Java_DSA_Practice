d3636c937fc132a814f07c11f4939ab6bda7e9c7
Ateeq <mdateequrrahma123@gmail.com>
2026-01-12 22:36:36 +0530

    closest element in BST problem added

---
+++ BST/closestEleinBST.java
@@ -0,0 +1,41 @@
+public class closestEleinBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static int closestVal(Node root, int target) {
+        int closest = root.data;
+
+        while (root != null) {
+            if(Math.abs(root.data - target) < Math.abs(closest - target)) {
+                closest = root.data;
+            }
+
+            if(target < root.data) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+
+        root.right = new Node(11);
+        root.right.right = new Node(20);
+
+        int k = 2;
+
+        System.out.println(closestVal(root, k));
+    }
+}
