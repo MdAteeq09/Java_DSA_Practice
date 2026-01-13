5d11657909f298d4591ea0d68a78b3a7c5723de1
Ateeq <mdateequrrahma123@gmail.com>
2026-01-13 23:30:33 +0530

    merge two bst problem added

--- BST/mergeTwoBST.java
+++ BST/mergeTwoBST.java
@@ -80,12 +80,7 @@ public class mergeTwoBST {
     } 
 
     public static void main(String[] args) {
-        /*
-         * 2
-         * / \
-         * 1 4
-         * BST1
-         */
+        
         Node root1 = new Node(2);
         root1.left = new Node(1);
         root1.right = new Node(4);
