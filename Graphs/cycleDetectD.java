a9f0c24f907a1a3ee1aecc3eafd5d92374fcfafb
Ateeq <mdateequrrahma123@gmail.com>
2026-01-28 21:42:30 +0530

    detect cycle in directed graph problem added

--- Graphs/cycleDetectD.java
+++ Graphs/cycleDetectD.java
@@ -27,12 +27,12 @@ public class cycleDetectD {
 
         
         // no cycle
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+        // graph[0].add(new Edge(0, 1));
+        // graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 3));
+        // graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2, 3));
+        // graph[2].add(new Edge(2, 3));
 
     }
 
