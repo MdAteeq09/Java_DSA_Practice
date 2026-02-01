92b5a2eacadb071b9f5083150ae716e9641e47ab
Ateeq <mdateequrrahma123@gmail.com>
2026-02-01 23:59:21 +0530

    disjointSetUnion problem added

--- Graphs/disjointSetUnion.java
+++ Graphs/disjointSetUnion.java
@@ -40,6 +40,6 @@ public class disjointSetUnion {
         union(1, 4);
         System.out.println(find(3));
         System.out.println(find(4));
-        union(1, 5);
+        union(1, 5);//union
     }
 }
