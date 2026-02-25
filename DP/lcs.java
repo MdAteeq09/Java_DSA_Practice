44e085401354aa1492251f37d6b60182ddd06fdd
Ateeq <mdateequrrahma123@gmail.com>
2026-02-25 23:51:56 +0530

    lcs problem added

--- DP/lcs.java
+++ DP/lcs.java
@@ -62,19 +62,19 @@ public class lcs {
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abedg";
-        // int n = str1.length();
-        // int m = str2.length();
-        // int dp[][] = new int[n+1][m+1];
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n+1][m+1];
 
-        // //initilization
-        // for(int i=0; i<n+1; i++) {
-        //     for(int j=0; j<m+1; j++) {
-        //         dp[i][j]= -1;
-        //     }
-        // }
+        //initilization
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<m+1; j++) {
+                dp[i][j]= -1;
+            }
+        }
        
-        // System.out.println(lcsm(str1, str2, n, m, dp));
+        System.out.println(lcsm(str1, str2, n, m, dp));
 
-        System.out.println(lcsTab(str1, str2));
+        // System.out.println(lcsTab(str1, str2));
     }
 }
