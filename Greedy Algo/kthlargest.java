23c92f7a3f88981b5b9a46b5b19063e6a85a2182
Ateeq <mdateequrrahma123@gmail.com>
2026-03-18 23:46:17 +0530

    kth largest odd no problem added

---
+++ Greedy Algo/kthlargest.java
@@ -0,0 +1,16 @@
+public class kthlargestOddNo {
+    public static int largest(int L, int R, int K) {
+        int fO = (R % 2 == 1) ? R : ( R - 1);
+        int ans = fO - 2 * (K - 1);
+        
+        if(ans < K) {
+            return 0;
+        }
+        
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(largest(3,13,1));
+    }
+}
