2ca45f07d36544cb405c13399835fd450bf914ec
Ateeq <mdateequrrahma123@gmail.com>
2026-03-28 23:43:34 +0530

    hash set opp problem added

---
+++ Hashing/hashSetOpp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hashSetOpp {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        hs.add(1);
+        hs.add(2);
+        hs.add(4);
+        hs.add(2);
+        hs.add(1);
+
+        System.out.println(hs);
+        hs.clear();
+        System.out.println(hs.isEmpty());
+        System.out.println(hs.size());
+    }
+}
