4b1fa473f347f58f76437188de68cf921a1f73f1
Ateeq <mdateequrrahma123@gmail.com>
2026-04-01 23:10:34 +0530

    linked hashmap problem added

---
+++ Hashing/linkedHashmap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class linkedHashmap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+
+        lhm.put("India", 100);
+        lhm.put("Japan", 150);
+        lhm.put("USA", 200);
+        lhm.put("China", 250);
+
+        System.out.println(lhm);
+    }
+}
