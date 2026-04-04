7ea17fe56217f82d4521c4afb72e0a199314890e
Ateeq <mdateequrrahma123@gmail.com>
2026-04-04 23:45:17 +0530

    sub array sum equal to array prblem added

--- Hashing/subArrSumEqalToK.java
+++ Hashing/subArrSumEqalToK.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class subArrSumEqalToK {
     public static void main(String[] args) {
-        int arr[] = {10, 2, -2, -20, 11};
+        int arr[] = {10, 2, -2, -20, 10};
         int k = -10;
 
         HashMap<Integer, Integer> map = new HashMap<>();
