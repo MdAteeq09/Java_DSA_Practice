46fcee50ea1076919ec918cd83b546093c98df2a
Ateeq <mdateequrrahma123@gmail.com>
2026-04-06 23:07:31 +0530

    Tree map code added

--- Hashing/treeMap.java
+++ Hashing/treeMap.java
@@ -5,7 +5,7 @@ public class treeMap {
         TreeMap<String, Integer> tm = new TreeMap<>();
 
         tm.put("Japan", 150);
-        tm.put("India", 100);
+        tm.put("India", 1000);
         tm.put("USA", 200);
         tm.put("China", 250);
 
