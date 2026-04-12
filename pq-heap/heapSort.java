b4241574ddf51150f90632df1e2c0277939411f9
Ateeq <mdateequrrahma123@gmail.com>
2026-04-12 23:58:51 +0530

    heap sort added

--- pq-heap/heapSort.java
+++ pq-heap/heapSort.java
@@ -49,7 +49,7 @@ public class heapSort {
 
         // print
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "  ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
