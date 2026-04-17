65d7a6d8ac967d3f5bd26ca791fcd0a4ccaa77d8
Ateeq <mdateequrrahma123@gmail.com>
2026-04-17 23:03:09 +0530

    priority queue added

--- pq-heap/pq.java
+++ pq-heap/pq.java
@@ -1,3 +1,37 @@
-public . pq {
-    
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class pq {
+
+    static class Student implements Comparable<Student> {
+        int rank;
+        String name;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+
+    public static void main(String[] args) {
+        // PriorityQueue <Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+
+        pq.add(new Student("A", 4)); // O(logn)
+        pq.add(new Student("B", 5));
+        pq.add(new Student("c", 2));
+        pq.add(new Student("D", 12));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank); // O(1)
+            pq.remove();// O(logn)
+        }
+
+    }
 }
