6f4395b6aa9db40dd597d65d2c45a1e36cb825b5
Ateeq <mdateequrrahma123@gmail.com>
2026-04-28 23:11:46 +0530

    minCostRopes added

--- Queue/minCostRopes.java
+++ Queue/minCostRopes.java
@@ -1,3 +1,30 @@
+import java.util.*;
+
 public class minCostRopes {
+    public static int min( int arr[], int n) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++) {
+            pq.add(arr[i]);
+        }
+
+        int ans = 0;
+
+        while(pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            int sum = first + second;
+            ans += sum;
+            pq.add(sum);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+        int n = 3;
+        System.out.println(min(arr, n));
+    }
     
 }
