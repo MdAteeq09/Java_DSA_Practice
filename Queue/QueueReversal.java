d6416997373fc574a2ef9dda3fcb48eff4a58d86
Ateeq <mdateequrrahma123@gmail.com>
2026-04-29 22:46:08 +0530

    Queue reversal added

--- Queue/QueueReversal.java
+++ Queue/QueueReversal.java
@@ -14,9 +14,9 @@ public class QueueReversal {
     }
 
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> qu = new LinkedList<>();
         q.add(1);
-        q.add(12);
+        q.add(2);
         q.add(3);
         q.add(4);
         q.add(5);
