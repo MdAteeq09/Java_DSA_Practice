fe9d418b83abb617d2efb508cfda37ddc4eb173c
Ateeq <mdateequrrahma123@gmail.com>
2026-05-04 21:32:31 +0530

    queueusingJcf added

--- Queue/queueusingJcf.java
+++ Queue/queueusingJcf.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class queueusingJcf {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
-        // Queue<Integer> q = new ArrayDeque<>();
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
 
         q.add(1);
         q.add(2);
